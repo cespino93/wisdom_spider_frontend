a1bc74db00934544adb59c89c10aea0fced775de
cespino93 <chrisl.espino@gmail.com>
2022-01-22 17:41:47 -0500

    errors

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 const endPoint = "http://localhost:3000/api/v1/websites"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getWebsites()
-    const createWebsiteForm = document.querySelector ("#create-website-form")
-    createWebsiteForm.addEventListener("submit", (e) => createFormHandler(e))
+  getWebsites()
+  const createWebsiteForm = document.querySelector("#create-website-form")
+  createWebsiteForm.addEventListener("submit", (e) => createFormHandler(e))
 })
 
-document.addEventListener("click", function(e) {
-    const websiteCard = document.getElementById(`${e.target.dataset.id}`)
-    if(e.target.matches("#delete-btn")) {
-      e.preventDefault()
-      deleteWebsite(e.target.dataset.id)
-      websiteCard.remove(websiteCard)
-    }
+document.addEventListener("click",function(e) {
+  const websiteCard = document.getElementById('${e.target.dataset.id}')
+  if(e.target.matches("delete-btn")){
+    e.preventDefault()
+    deleteWebsite(e.target.dataset.id)
+    websiteCard.remove(websiteCard)
+  }
 })
 
-function getWebsites() {
-    fetch(endPoint)
-    .then(response => response.json())
-    .then(websites => {
+function getWebsites(){
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(websites => {
     websites.data.forEach(website => {
-        let newWebsite = new Website(website, website.attributes)
-        document.querySelector('#website-container').innerHTML += newWebsite.renderWebsite()
-      })
+      let newWebsite = new Website(website, website.attributes)
+      document.querySelector('#website-container').innerHTML +=
+      newWebsite.renderWebsite()
     })
+  })
 }
 
 function createFormHandler(e) {
-    e.preventDefault()
-    const titleInput = document.querySelector('#input-title').value
-    const descriptionInput = document.querySelector('#input-description').value
-    const imageInput = document.querySelector('#input-url').value
-    const categoryId = parseInt(document.querySelector('#categories').value)
-    postWebsites (titleInput, descriptionInput, imageInput, categoryId)
+  e.preventDefault()
+  const titleInput = document.querySelector('#input-title').value
+  const descriptionInput = document.querySelector('#input-description').value
+  const imageInput = document.querySelector('#input-url').value
+  const categoryId = parseInt(document.querySelector('#categories').value)
+  postWebsites (titleInput,descriptionInput, imageInput, categoryId)
 }
 
 function postWebsites(title, description, image_url, category_id) {
-    let bodyObj = {title, description, image_url, category_id}
-    fetch(endPoint, {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(bodyObj)
+  letbodyObj = {title, description, image_url, category_id}
+  fetch(endPoint, {
+    method: "POST",
+    headers:{"Content-Tpe": "application/json"},
+    body: JSON.stringify(bodyObj)
   })
-    .then(response => response.json())
-    .then(website => {
-        const websiteData = website.data
-        let newWebsite = new Website(websiteData, websiteData.attributes)
-
-        document.querySelector('#website-container').innerHTML += newWebsite.renderWebsite()
+  .then(response => response.json())
+  .then(website => {
+    const websiteData = website.data.mapletnewWebsite = new Website(websiteData, websiteData.attributes)
+    document.querySelector('#website-container').innerHTML +=
+    newWebsite.renderWebsite()
   })
 }
 
 function deleteWebsite(id) {
-   fetch(`${endPoint}/${id}`, {
-      method: "DELETE" 
+  fetch('${endPoint}/${id}',{
+    method: "DELETE"
   })
-    .then(response => response.json())
+  .then(response => response.json())
 }
--- website.js
+++ website.js
@@ -1,4 +1,5 @@
 class Website {
+  
   constructor(website, websiteAttributes) {
     this.id = website.id
     this.title = websiteAttributes.title
@@ -6,17 +7,19 @@ class Website {
     this.image_url = websiteAttributes.image_url
     this.category = websiteAttributes.category
       Website.all.push(this)
-    }
-  renderWebsite() {
-    return `
-    <div data-id=${this.id}>
-    <h3>${this.title}</h3>
-    <img src=${this.image_url} height="300" width="350">
-    <p><strong>Description:</strong> ${this.description}</p>
-    <p><strong>Category:</strong> ${this.category.name}</p>
-    <button data-id=${this.id} id="delete-btn" type="button" class="delete-btn">Delete Website</button>
-    </div>    
-    <br><br>`;     
-    }
+      console.log(this);
   }
+
+renderWebsite() {
+  return `
+  <div data-id=${this.id}>
+  <h3>${this.title}</h3>
+  <img src=${this.image_url} height="300" width="350">
+  <p><strong>Description:</strong> ${this.description}</p>
+  <p><strong>Category:</strong> ${this.category.name}</p>
+  <button data-id=${this.id} id="delete-bin" type="button" class="delete-btn">Delete Website</button>
+  </div>
+  <br><br>`;
+  }
+}
 Website.all = [];
